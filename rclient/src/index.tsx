6d6b5a856ba822ead6a367b71b3ba25b099fab31
Yosef Shachnovsky <yosefshachnovsky@Yosefs-MacBook-Air.local>
2025-01-22 14:12:51 +0200

    added todo in index file

--- rclient/src/index.tsx
+++ rclient/src/index.tsx
@@ -22,3 +22,20 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+/**
+ * TODO: 0. create time line for the project
+ * 1. remove redundant components such as comment box, and the like, they are here to demonstrate redux
+ * 2. remove the redux store and the provider
+ * 3. add docker file to server
+ * 4. figure out how to modify env variables in docker for client and seerver
+ * 4. add docker kubernetes to server and client
+ * 5. add tests to server
+ * 6. add tests to client
+ * 7. add tests to crdt on server side
+ * 8. add ci/cd to server
+ * 9. add ci/cd to client
+ * 10. add tests to ci/cd pipeline
+ * 11. add persistent storage to server implenentation
+ *
+ */
