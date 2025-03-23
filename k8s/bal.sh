4afe61a5414b7224018e010fdfd21bf6f0f32ef6
Yosef Shachnovsky <yosefshachnovsky@Yosefs-MacBook-Air.local>
2025-03-23 17:38:28 +0200

    working version dockers locally

---
+++ k8s/bal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+{
+    "Policy": {
+        "PolicyName": "AWSLoadBalancerControllerIAMPolicy",
+        "PolicyId": "ANPA6ATCOUXPUW4ZNH2QE",
+        "Arn": "arn:aws:iam::963352896991:policy/AWSLoadBalancerControllerIAMPolicy",
+        "Path": "/",
+        "DefaultVersionId": "v1",
+        "AttachmentCount": 0,
+        "PermissionsBoundaryUsageCount": 0,
+        "IsAttachable": true,
+        "CreateDate": "2025-03-18T16:05:44+00:00",
+        "UpdateDate": "2025-03-18T16:05:44+00:00"
+    }
+}
---
+++ k8s/helm-install-loadbalancer.shj
@@ -0,0 +1,1 @@
+ helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName=crdt-demo-cluster --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller
