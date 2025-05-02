d1ea923227e063d3825c270c34540acffccf1514
Yosef Shachnovsky <yosefshachnovsky@Yosefs-MacBook-Air.local>
2025-05-02 13:13:28 +0300

    added secret folder

---
+++ server/jest.config.js
@@ -0,0 +1,33 @@
+// filepath: /Users/yosefshachnovsky/dev/crdtdemo/server/jest.config.js
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testPathIgnorePatterns: ["/node_modules/", "/build/"], // Exclude the build directory
+};
+/*
+This tells Jest to use ts-jest as the preset.
+ts-jest is a Jest transformer specifically designed for TypeScript. It compiles TypeScript files to JavaScript before running the tests.
+This is required to handle .ts and .tsx files in your project.
+testEnvironment: "node"
+
+Specifies the environment in which your tests will run.
+"node" means the tests will run in a Node.js environment, which is suitable for backend projects or server-side code.
+If you're testing browser-based code, you might use "jsdom" instead.
+transform
+
+Specifies how Jest should transform files before running tests.
+The key is a regular expression (^.+\\.tsx?$) that matches all .ts and .tsx files.
+The value ("ts-jest") tells Jest to use ts-jest to transform these files.
+moduleFileExtensions
+
+Specifies the file extensions Jest should look for when resolving modules.
+This list includes:
+ts and tsx: TypeScript files.
+js and jsx: JavaScript files.
+json: JSON files.
+node: Node.js modules.
+*/
--- server/src/__tests__/crdt.test.ts
+++ server/src/__tests__/crdt.test.ts
@@ -1,30 +1,32 @@
-import {CRDT, LWWMap, LWWRegister, State }  from '../crdt/CRDTTypes'; 
+import { CRDT, LWWMap, LWWRegister, State } from "../crdt/CRDTTypes";
 
-describe('CRDT', () => {
-  it('should be able to create a CRDT object', () => {
-    const crdt = new LWWRegister('aa', ['aa', 1, 'bb']);
-    expect(crdt).toBeDefined();
-    console.log('yyeeeehawww');
-  });
+describe.skip("CRDT", () => {
+  // it("should be able to create a CRDT object", () => {
+  //   const crdt = new LWWRegister("aa", ["aa", 1, "bb"]);
+  //   expect(crdt).toBeDefined();
+  //   console.log("yyeeeehawww");
+  // });
 
-  it('should be able to create a CRDT object', () => {
-    const crdt1 = new LWWRegister('1', ['aa', 1, 'bb']);
-    const crdt2 = new LWWRegister('c', ['aa', 2, 'cc']);
+  it("should be able to create a CRDT object", () => {
+    const crdt1 = new LWWRegister("1", ["aa", 1, "bb"]);
+    const crdt2 = new LWWRegister("c", ["aa", 2, "cc"]);
     crdt1.merge(crdt2.state);
-    expect(crdt1.value).toEqual('cc');
-   
+    expect(crdt1.value).toEqual("cc");
+    console.log("ooooooooooooo");
   });
+
+  // this is a test that creates lwwmap object and merges to anothe lwwmap object
+  // test lwwmap merge function
 
   // test lwwmap merge function
-  it('should be able to merge two lwwmap objects', () => {
-    const id  = 'lww1';
-    const state1: State<string> = { a: [id, 1, 'ab'], b: [id, 2, 'bb'] };
+  it("should be able to merge two lwwmap objects", () => {
+    const id = "lww1";
+    const state1: State<string> = { a: [id, 1, "ab"], b: [id, 2, "bb"] };
     const lwwmap1 = new LWWMap(id, state1);
-    const id2 = 'lww2';
-    const state2: State<string> = { a: [id2, 2, 'aa'], c: [id2, 1, 'cc'] };
+    const id2 = "lww2";
+    const state2: State<string> = { a: [id2, 2, "aa"], c: [id2, 1, "cc"] };
     const lwwmap2 = new LWWMap(id2, state2);
     lwwmap1.merge(lwwmap2.state);
-    expect(lwwmap1.value).toEqual({ a: 'aa', b: 'bb', c: 'cc' });
-    
+    expect(lwwmap1.value).toEqual({ a: "aa", b: "bb", c: "cc" });
   });
 });
