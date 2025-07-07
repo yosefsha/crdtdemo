edb77f88825c8a18b3a1211538000183880eec6e
Yosef Shachnovsky <sh.yosef@gmail.com>
2025-07-07 22:20:32 +0300

    removed comments logic and added welcome page

---
+++ rclient/src/components/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the CRDT Demo</h1>
+      <p>This is a collaborative drawing application using CRDTs.</p>
+    </div>
+  );
+};
+
+export default Welcome;
